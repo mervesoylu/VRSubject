8e1e609158f9a8ebd4f85b9886f49540648ba2ab
Sefa <sefanursoyludesign@gmail.com>
2020-06-01 13:03:44 +1000

    Added oxygen timer

---
+++ Assets/Scripts/oxygenTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class oxygenTimer : MonoBehaviour
+{
+    public Image oxgTimer;
+    public float timer;
+    public float maxTime = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        var percent = timer / maxTime;
+        oxgTimer.fillAmount = Mathf.Lerp(0, 1, percent);
+    }
+}
