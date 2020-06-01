63739bb254dfff4e20b471b2b25f742eab13e07b
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2020-06-01 12:15:21 +1000

    Interactable Objects now fly towards player when they find it, objects float around, when player looks at object it stops floating

---
+++ Assets/Scripts/InteractionBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 2;
+    private bool buttonPressed = false;
+
+    public void ButtonClick()
+    {
+        buttonPressed = true;
+    }
+
+    public void OnEnter()
+    {
+       var fscript = GetComponent<floater>();
+        fscript.enabled = false;
+    }
+
+    public void OnExit()
+    {
+       var fscript = GetComponent<floater>();
+        fscript.enabled = true;
+    }
+
+    private void Update()
+    {
+        if(buttonPressed == true)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+            var fscript = GetComponent<floater>();
+            fscript.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectHandler.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHandler : MonoBehaviour
+{
+    public GameObject[] ObjectsToFind;
+}
