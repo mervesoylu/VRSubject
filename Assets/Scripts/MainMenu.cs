709c0d6bba6803fb994105cd74717d6caecac0b1
Sefa <sefanursoyludesign@gmail.com>
2020-06-02 12:11:05 +1000

    made the main menu with animations and added skybox materials

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject doorLight;
+    public bool doorClicked = false;
+    public Animator doorOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorClicked == true)
+        {
+            doorOpen.SetBool("isOpen", true);
+        }
+    }
+
+    public void OnClick()
+    {
+        doorLight.GetComponent<Renderer>().material.color = Color.green;
+        doorLight.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.green);
+        doorClicked = true;
+    }
+
+}
