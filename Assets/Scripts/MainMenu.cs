365de055f41c7ae64fc6ecaf8292a6f8fecc3666
Sefa <sefanursoyludesign@gmail.com>
2020-06-02 13:32:10 +1000

    added fade to black to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject doorLight;
     public bool doorClicked = false;
     public Animator doorOpen;
+    public float timer;
+    public float delay = 5;
+    public GameObject panel;
+    public float sceneLoad = 8;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +25,19 @@ public class MainMenu : MonoBehaviour
         if (doorClicked == true)
         {
             doorOpen.SetBool("isOpen", true);
+            timer += Time.deltaTime;
         }
+
+        if (timer >= delay)
+        {
+            panel.SetActive(true);
+        }
+
+        if (timer >= sceneLoad)
+        {
+            SceneManager.LoadScene(1);
+        }
+
     }
 
     public void OnClick()
