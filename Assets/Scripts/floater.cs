c31f9ddead66302e48ecaec409b06e3bba159550
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2020-06-03 10:37:35 +1000

    Added Randomness to Floating

--- Assets/Scripts/floater.cs
+++ Assets/Scripts/floater.cs
@@ -10,9 +10,11 @@ using System.Collections;
 public class floater : MonoBehaviour
 {
     // User Inputs
-    public float degreesPerSecond = 15.0f;
-    public float amplitude = 0.5f;
+    public float degreesPerSecond = 0;
+    public float amplitude = 0;
     public float frequency = 1f;
+
+    private float timeOffset = 0;
 
     // Position Storage Variables
     Vector3 posOffset = new Vector3();
@@ -23,6 +25,10 @@ public class floater : MonoBehaviour
     {
         // Store the starting position & rotation of the object
         posOffset = transform.position;
+
+        timeOffset = Random.Range(0, 10);
+        amplitude = Random.Range(0.1f, 0.7f);
+        degreesPerSecond = Random.Range(15, 30);
     }
 
     // Update is called once per frame
@@ -33,7 +39,7 @@ public class floater : MonoBehaviour
 
         // Float up/down with a Sin()
         tempPos = posOffset;
-        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency + timeOffset) * amplitude;
 
         transform.position = tempPos;
     }
