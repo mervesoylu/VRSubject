9680715f5de095bade065086a0b4ccec5ce12c19
Sefanur <sefanursoyludesign@gmail.com>
2020-06-10 13:57:46 +1000

    made it so that the player can go back to the main menu by clicking onto the space ship

---
+++ Assets/Scripts/spaceShuttleInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class spaceShuttleInteraction : MonoBehaviour
+{
+    public Button yes;
+    public Button no;
+
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    public void OnClickSHuttle()
+    {
+        panel.SetActive(true);
+    }
+    
+    public void OnClickYes()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnClickNo()
+    {
+        panel.SetActive(false);
+    }
+}
