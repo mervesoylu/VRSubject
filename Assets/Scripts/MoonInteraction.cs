2ec3008c9c41ca6b646e3cf75d3047f84c5cabf2
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2020-06-13 15:30:46 +1000

    Added moon jumping

--- Assets/Scripts/MoonInteraction.cs
+++ Assets/Scripts/MoonInteraction.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class MoonInteraction : MonoBehaviour
 {
     public GameObject Player;
-    public GameObject target;
+    public Transform target;
+
+    public float speed = 2;
+    private bool isPressed = false;
 
     public void OnClick()
     {
-        //when the player presses the button on another moon, they are teleported to a target gameobject on that moon
-        Player.transform.position = target.transform.position;
+        isPressed = true;
+    }
+
+    void Update()
+    {
+        if(isPressed == true)
+        {
+            //when the player presses the button on another moon, they move towards the target gameobject on that moon
+            float step = speed * 100 * Time.deltaTime;
+            Player.transform.position = Vector3.MoveTowards(Player.transform.position, target.position, step);
+        }
+
+        if(Player.transform.position == target.position)
+        {
+            isPressed = false;
+        }
+       
     }
 }
