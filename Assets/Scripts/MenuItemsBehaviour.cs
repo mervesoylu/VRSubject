198a2164cac8c23a334852fac6494763d6b59985
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2020-06-15 13:22:22 +1000

    Added items to main menu

---
+++ Assets/Scripts/MenuItemsBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItemsBehaviour : MonoBehaviour
+{
+    public GameObject door;
+    public Transform flyTo;
+    private bool newPosition = true;
+    private bool fly = false;
+
+    private float speed;
+
+    private float randX;
+    private float randY;
+    private float randZ;
+
+    void Start()
+    {
+        randX = Random.Range(-5, 5);
+        randY = Random.Range(-0.1f, 0.1f);
+        randZ = Random.Range(-5, 5);
+        speed = Random.Range(5f, 10f);
+    }
+
+    void Update()
+    {
+        if ((door.GetComponent<MainMenu>().doorClicked == true) && (newPosition == true))
+        {
+            transform.position = new Vector3(randX, randY, randZ);
+            fly = true;
+            newPosition = false;
+        }
+
+        if (fly == true)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, flyTo.position, step);
+        }
+    }
+}
