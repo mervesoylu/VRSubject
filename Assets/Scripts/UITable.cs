bd4eb45e8e3c50204f528205827b55cafeeeabb1
mervesoylu <48895408+mervesoylu@users.noreply.github.com>
2020-06-20 18:56:38 +1000

    Adjusted UI so it is no longer a post it stuck to player's helmet

---
+++ Assets/Scripts/UITable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITable : MonoBehaviour
+{
+    public GameObject player;
+
+
+    void Update()
+    {
+
+        Vector3 eulerRotation = new Vector3(transform.eulerAngles.x, player.transform.eulerAngles.y, transform.eulerAngles.z);
+
+        transform.rotation = Quaternion.Euler(eulerRotation);
+    }
+}
